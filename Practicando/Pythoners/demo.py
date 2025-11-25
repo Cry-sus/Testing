a359734ded4cdaa45dbb0783ee63e87138f2d9a6
Cry-sus <101567916+Cry-sus@users.noreply.github.com>
2025-11-25 19:50:52 -0300

    Agregnado el contenido de mis proeyctos de bootcamp python fullstack 25/11/2025

---
+++ Practicando/Pythoners/demo.py
@@ -0,0 +1,14 @@
+x = 5
+
+def verificar_paridad(n):
+    if n % 2 == 0:
+        return f"{n} es un numero par"
+    else:
+        return f"{n} es un numero impar"
+    
+
+    
+print(verificar_paridad(x))
+
+# for i in range(x):
+#     print(i)
